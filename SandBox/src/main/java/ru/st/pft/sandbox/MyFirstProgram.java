6d0dabbceac7e54bbd89630900de95af529f658f
Andrey Sokolovskiy <bugnetsys@gmail.com>
2019-02-27 19:17:48 +0300

    FIx1

--- SandBox/src/main/java/ru/st/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/st/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.st.pft.sandbox;
 
 public class MyFirstProgram {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
         System.out.println("Ready");
   }
 }
