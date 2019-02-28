c33a2718197ef66b09db9f9d349b4fd461ef3adf
Andrey Sokolovskiy <bugnetsys@gmail.com>
2019-02-28 13:27:24 +0300

    Methods added

--- SandBox/src/main/java/ru/st/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/st/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,23 @@ package ru.st.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-        System.out.println("Ready");
+    hello("Andrey");
+    int lenA = 5;
+    int lenB = 3;
+
+    System.out.println("Rectangle Square is " + area(lenA));
+    System.out.println("Rectangle Square is " + area(lenA, lenB));
+  }
+
+  public static void hello(String someBody) {
+    System.out.println("Hello," + someBody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double lenA, double lenB) {
+    return lenA * lenB;
   }
 }
